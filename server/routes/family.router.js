c1725c41f546f7c4475582a34c1be9cad357cc68
Bryan Highhill <bryanhighhill@gmail.com>
2023-02-25 14:21:13 -0600

    started work on a checkbox element next to recipe instruction - Goal: have checkbox checked status true change className so that css can be used to lower opacity of that item

--- server/routes/family.router.js
+++ server/routes/family.router.js
@@ -32,7 +32,7 @@ router.post('/', (req, res) => {
       VALUES ($1) RETURNING id`;
     pool.query(queryText, [req.body.name])
     .then((query) => {
-      pool.query('UPDATE "user" SET "family_id" = $1 WHERE "id" = $2', [query.rows[0].id, req.body.user_id])
+      pool.query('UPDATE "user" SET "family_id" = $1, "admin" = true WHERE "id" = $2;', [query.rows[0].id, req.body.user_id])
       .then(() => {
         res.sendStatus(201)
       })
