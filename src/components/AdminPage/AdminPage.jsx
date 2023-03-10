d55078225af3eedda8c5f1d248e8d3f6bf6a929a
Bryan Highhill <bryanhighhill@gmail.com>
2023-03-09 21:24:21 -0600

    fixed the positioning of the delete modals, both on Admin page and on recipe page. Changing the position: fixed corrected the issue of the modal not appearing on page if you were scrolled down to the bottom on a longer page

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -24,18 +24,18 @@ const AdminPage = () => {
     }
 
     const openModal = (member) => {
-        const modal = document.querySelector('.modal');
-        const overlay = document.querySelector('.overlay');
-        modal.classList.remove('hidden'); //removes hidden class from modal
-        overlay.classList.remove('hidden'); //removes hidden class from overlay
+        const modal = document.querySelector('.modal2');
+        const overlay = document.querySelector('.overlay2');
+        modal.classList.remove('hidden2'); //removes hidden class from modal
+        overlay.classList.remove('hidden2'); //removes hidden class from overlay
         setDeleteID(member.id);
     }
 
     const closeModal = () => {
-        const modal = document.querySelector('.modal');
-        const overlay = document.querySelector('.overlay');
-        modal.classList.add('hidden'); //adds hidden class to modal
-        overlay.classList.add('hidden'); //adds hidden class to modal
+        const modal = document.querySelector('.modal2');
+        const overlay = document.querySelector('.overlay2');
+        modal.classList.add('hidden2'); //adds hidden class to modal
+        overlay.classList.add('hidden2'); //adds hidden class to modal
         setDeleteID(0);
     }
 
@@ -99,57 +99,55 @@ const AdminPage = () => {
                                     const date = new Date(member.registration_date);
 
                                     return (
-                                        <>
-                                            <div className={member.username != user.username ? "family-member-container" : "your-container"}>
-                                                <b><u>{member.username}</u></b>
-                                                <br />
-                                                <br />
-                                                <b>registered on:</b>
-                                                <br />
-                                                {(date.toLocaleDateString("en-US", options))} {/* date format string */}
-                                                <br />
-                                                <br />
-                                                <b>status:</b>
-                                                <br />
-                                                {member.admin ? 'admin' : 'not admin'}
-                                                <br />
-                                                <br />
-                                                <b>contact:</b> 
-                                                <br />
-                                                {member.email}
-                                                <br />
-                                                {member.username != user.username &&
-                                                    <>
-                                                        <br />
-                                                        <button className="btn_sizeMed" onClick={() => {admin(member)}}>{member.admin ? 'remove admin' : 'make admin'}</button>
-                                                        <br />
-                                                        <br />
-                                                        {/* <button className="btn_delete" onClick={() => removeFromFamily(member.id)}>Remove</button> */}
-                                                        <section className="modal hidden"> {/* modal container */}
-                                                            <div className="flex">
-                                                                <button onClick={closeModal} className="btn_close_modal">X</button>
-                                                            </div>
-                                                            <div>
-                                                                <h2>Are you sure you want to remove this user from your family?</h2>
-                                                            </div>
-                                                            <div className="modal_delete_container">
-                                                                <button onClick={clickHandler} className="btn_modal_delete">Delete</button>
-                                                            </div>
-                                                        </section>
+                                        <div className={member.username != user.username ? "family-member-container" : "your-container"}>
+                                            <b><u>{member.username}</u></b>
+                                            <br />
+                                            <br />
+                                            <b>registered on:</b>
+                                            <br />
+                                            {(date.toLocaleDateString("en-US", options))} {/* date format string */}
+                                            <br />
+                                            <br />
+                                            <b>status:</b>
+                                            <br />
+                                            {member.admin ? 'admin' : 'not admin'}
+                                            <br />
+                                            <br />
+                                            <b>contact:</b> 
+                                            <br />
+                                            {member.email}
+                                            <br />
+                                            {member.username != user.username &&
+                                                <>
+                                                    <br />
+                                                    <button className="btn_sizeMed" onClick={() => {admin(member)}}>{member.admin ? 'remove admin' : 'make admin'}</button>
+                                                    <br />
+                                                    <br />
+                                                    {/* <button className="btn_delete" onClick={() => removeFromFamily(member.id)}>Remove</button> */}
+                                                    <section className="modal2 hidden2"> {/* modal container */}
+                                                        <div className="flex2">
+                                                            <button onClick={closeModal} className="btn_close_modal2">X</button>
+                                                        </div>
+                                                        <div>
+                                                            <h2>Are you sure you want to remove this user from your family?</h2>
+                                                        </div>
+                                                        <div className="modal_delete_container2">
+                                                            <button onClick={clickHandler} className="btn_modal_delete2">Delete</button>
+                                                        </div>
+                                                    </section>
 
-                                                        <div className="overlay hidden"></div> {/* overlay element - dark blurred background when modal is open */}
-                                                        <button
-                                                            className="btn_delete"
-                                                            onClick={() => openModal(member)}
-                                                        >
-                                                            Remove
-                                                        </button>
-                                                    </>
-                                                }
-                                                <br />
-                                                <br />
-                                            </div>
-                                        </>
+                                                    <div className="overlay2 hidden2"></div> {/* overlay element - dark blurred background when modal is open */}
+                                                    <button
+                                                        className="btn_delete"
+                                                        onClick={() => openModal(member)}
+                                                    >
+                                                        Remove
+                                                    </button>
+                                                </>
+                                            }
+                                            <br />
+                                            <br />
+                                        </div>
                                     )
                                 })}
                             </>
--- src/components/RegisterPage/RegisterPage.jsx
+++ src/components/RegisterPage/RegisterPage.jsx
@@ -7,20 +7,22 @@ function RegisterPage() {
   const history = useHistory();
 
   return (
-    <div>
-      <RegisterForm />
+    <div className="login-page">
+      <div className="login-form">
+        <RegisterForm />
 
-      <center>
-        <button
-          type="button"
-          className="btn btn_asLink"
-          onClick={() => {
-            history.push('/login');
-          }}
-        >
-          Login
-        </button>
-      </center>
+        <center>
+          <button
+            type="button"
+            className="btn btn_asLink"
+            onClick={() => {
+              history.push('/login');
+            }}
+          >
+            Login
+          </button>
+        </center>
+      </div>
     </div>
   );
 }
